fb338faafec206bba2d225c87f4f0c211103e73c
hsk99 <3024186605@qq.com>
2022-01-22 16:50:26 +0800

    update

--- src/config/plugin/hsk99/push/app.php
+++ src/config/plugin/hsk99/push/app.php
@@ -3,7 +3,7 @@
 return [
     'enable'       => true,
     'debug'        => true,
-    'listen'       => 'Push://0.0.0.0:8803',
+    'listen'       => 'tcp://0.0.0.0:8803',
     'protocol'     => \Hsk99\WebmanPush\Protocols\Push::class,
     'api'          => 'http://0.0.0.0:3232',
     'app_key'      => 'APP_KEY_TO_REPLACE',
